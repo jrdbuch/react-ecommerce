cddce6aea31bcb925ebbcc902ae85b5d66267452
Jared Buchanan <jrdbuch@yahoo.com>
2022-01-24 00:02:11 -0800

    init commit

---
+++ src/components/directory.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Men
+
+const Directory = () => {
+    let x = 1;
+
+    return (
+        <div>
+            {x}
+        </div>
+    )
+}
+
+export default Directory;
+
---
+++ src/components/homepage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Directory from './directory'
+
+const Homepage = () => (
+    <div className='homepage'>
+        <Directory/>
+    </div>
+)
+
+export default Homepage;
